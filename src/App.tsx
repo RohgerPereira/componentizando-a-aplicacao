9636ca2c5632277635578ceef834a0d9c40dc20f
RohgerPereira <86059639+RohgerPereira@users.noreply.github.com>
2021-10-19 15:24:22 -0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import './styles/sidebar.scss';
 import './styles/content.scss';
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
+import { Provider } from './Context';
 
 export function App() {
   
   return (
+    <Provider>
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar />
       <Content />
 
     </div>
+    </Provider>
   )
 }
